9ee84cb09b81b55e40760d6fad4388ae86196d2d
kevinvilla <kevinvillab89@gmail.com>
2021-10-31 16:56:19 -0600

    todo funcionando, solo falta api ORDS

--- graf.js
+++ graf.js
@@ -1,16 +1,24 @@
 var ctx= document.getElementById("myChart").getContext("2d");
-        
+
+var eName=[], eSalary = [], eAge = [];
+var myChart;
+
 var myData = {  
-    labels:['Boston','Worcester','Springfield'],
+    labels: eName,
     datasets:[
         {
-            label:'Num datos',
-            data:[10,9,15],
-            backgroundColor:[
-                'rgb(66, 134, 244,0.5)',
-                'rgb(74, 135, 72,0.5)',
-                'rgb(229, 89, 50,0.5)'
-            ],
+            label:'Salary',
+            data:eSalary,
+            backgroundColor: generateColor(eSalary.length),
+            borderWidth: 2,
+            borderColor: '#777',
+            hoverBorderWidth: 3,
+            hoverBorderColor: '#000'
+        },
+        {
+            label:'Age',
+            data:eAge,
+            backgroundColor: generateColor(eAge.length),
             borderWidth: 2,
             borderColor: '#777',
             hoverBorderWidth: 3,
@@ -19,42 +27,7 @@ var myData = {
     ]
 };
 
-var myDataBars = {  
-    labels:['Cities'],
-    datasets:[
-        {
-            label:'Boston',
-            data:[10],
-            backgroundColor:[
-                'rgb(66, 134, 244,0.5)',
-            ],
-            borderWidth: 2,
-            borderColor: '#777',
-            hoverBorderWidth: 3,
-            hoverBorderColor: '#000'
-        },{
-            label:'Manchester',
-            data:[20],
-            backgroundColor:[
-                'rgb(74, 135, 72,0.5)',
-            ],
-            borderWidth: 2,
-            borderColor: '#777',
-            hoverBorderWidth: 3,
-            hoverBorderColor: '#000'
-        },{
-            label:'Springfield',
-            data:[12],
-            backgroundColor:[
-                'rgb(229, 89, 50,0.5)'
-            ],
-            borderWidth: 2,
-            borderColor: '#777',
-            hoverBorderWidth: 3,
-            hoverBorderColor: '#000'
-        }
-    ]
-};
+
 
 var myOptions = {
     title:{
@@ -75,24 +48,24 @@ var myOptions = {
     tooltips:{
         enabled: true
     }
-}
-
-
-                    
-var myChart= new Chart(ctx,{
-
-    type:"bar", // bar, horizontalBar, pie, line, radar, polarArea
-    data: myDataBars,
-    options: myOptions
-});
+};
 
 
 
 
 
+async function loadChart(){      
+    await getData();              
+        myChart= new Chart(ctx,{
+            type:"bar", // bar, horizontalBar, pie, line, radar, polarArea
+            data: myData,
+            options: myOptions
+    });
+};
 
 
-document.getElementById("legendBut").addEventListener("click", updateLegend);
+
+
 function updateLegend(){
     
     if (myChart.options.legend.display) {
@@ -102,11 +75,10 @@ function updateLegend(){
     };
 
     myChart.update();
-}
+};
 
 
 function updateChartType(){
-
     
 
     if (document.getElementById("chartType").value == "doughnut") {
@@ -120,7 +92,7 @@ function updateChartType(){
         myChart.destroy();
         myChart = new Chart(ctx, {
             type: document.getElementById("chartType").value,
-            data: myDataBars,
+            data: myData,
             options: myOptions
         });
     } else{
@@ -134,3 +106,44 @@ function updateChartType(){
         
 
 };
+
+
+async function getData(){
+    const url = "http://dummy.restapiexample.com/api/v1/employees";
+    const response = await fetch(url);
+    const barChartData = await response.json();
+    
+    
+    const salary = barChartData.data.map( (x) => x.employee_salary );
+    const age = barChartData.data.map( (x) => x.employee_age );
+    const name = barChartData.data.map( (x) => x.employee_name );
+
+    //CASO DUMMY API
+    myData.labels = name;
+    myData.datasets[0].data = salary;
+    myData.datasets[1].data = age;
+    myData.datasets[0].backgroundColor = generateColor(salary.length);
+    myData.datasets[1].backgroundColor = generateColor(age.length);
+
+};
+
+
+function random_rgba() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+};
+
+function generateColor(len){
+    let colors = []
+    console.log(len);
+    for (let index = 0; index < len; index++){
+       colors[index] = random_rgba();
+    }
+
+    console.log(colors);
+    return colors;
+};
+
+
+
+loadChart();
