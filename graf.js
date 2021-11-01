ad45d85d05a2a429dca56beb50bcdf22d9daa861
kevinvilla <kevinvillab89@gmail.com>
2021-11-01 11:30:31 -0600

    JSON format

--- graf.js
+++ graf.js
@@ -144,7 +144,8 @@ async function getData(){
         myData.datasets[2].backgroundColor = generateColor(apiLand.length);
 
         //json
-        document.getElementById("json").textContent = JSON.stringify(barChartData, null, 1)
+        document.getElementById("json").innerHTML = syntaxHighlight(JSON.stringify(barChartData, null, 1))
+
     }else{
         alert("Error")
     }
@@ -168,6 +169,25 @@ function generateColor(len){
     return colors;
 };
 
+
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
 
 
 loadChart();
